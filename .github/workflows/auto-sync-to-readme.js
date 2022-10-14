54f841d43eb854fd5deaf64f496f0b3137eeaace
trackiss <trackiss@hey.com>
2022-10-14 16:34:42 +0900

    Update auto-sync-to-readme.js

--- .github/workflows/auto-sync-to-readme.js
+++ .github/workflows/auto-sync-to-readme.js
@@ -17,6 +17,8 @@ exports.generatePreview = async ({ github, context, core }) => {
     return
   }
 
+  console.info('############### - ' + GITHUB_HEAD_REF);
+
   // 英数字以外はハイフンに置き換える
   const versionId = 'v2-' + String(GITHUB_HEAD_REF).replace(/[^a-zA-Z0-9]/g, '-')
 
