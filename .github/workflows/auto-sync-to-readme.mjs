db5ff733d1233ebb96e22ca4aebe6281eadd50d2
trackiss <trackiss@hey.com>
2022-10-17 10:14:03 +0900

    Update auto-sync-to-readme.mjs

--- .github/workflows/auto-sync-to-readme.mjs
+++ .github/workflows/auto-sync-to-readme.mjs
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch';
 import { fileFromSync } from 'fetch-blob/from.js'
 import { FormData } from 'formdata-polyfill/esm.min.js'
-import stream from 'node:stream';
 
 export async function generatePreview({ github, context, core }) {
   const { GITHUB_HEAD_REF } = process.env;
@@ -15,16 +14,17 @@ export async function generatePreview({ github, context, core }) {
     if (fetchVersionResponse.status === 404) {
       // ReadMeのバージョンを作成する
       const createVersionRequestJson = {
-        'version': versionId,
-        'codename': GITHUB_HEAD_REF,
-        'from': 'v2',
-        'is_stable': false,
-        'is_beta': false,
-        'is_hidden': true
+        version: versionId,
+        codename: GITHUB_HEAD_REF,
+        from: 'v2',
+        is_stable: false,
+        is_beta: false,
+        is_hidden: true
       }
 
-      await fetchReadMe('POST', '/version', JSON.stringify(createVersionRequestJson), {
-        'content-type': 'application/json'
+      await fetchReadMe('POST', '/version', {
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(createVersionRequestJson)
       })
         .then(response => Promise.all([response.ok, response.json()]))
         .then(([ok, json]) => {
@@ -37,7 +37,10 @@ export async function generatePreview({ github, context, core }) {
       const uploadOpenAPISpecFormData = new FormData();
       uploadOpenAPISpecFormData.append('spec', fileFromSync('./openapi/openapi.yaml'));
 
-      await fetchReadMe('POST', '/api-specification', uploadOpenAPISpecFormData, { 'x-readme-version': versionId })
+      await fetchReadMe('POST', '/api-specification', {
+        headers: { 'x-readme-version': versionId },
+        body: uploadOpenAPISpecFormData
+      })
         .then(response => Promise.all([response.ok, response.json()]))
         .then(([ok, json]) => {
           if (!ok) {
@@ -47,7 +50,9 @@ export async function generatePreview({ github, context, core }) {
     } else if (fetchVersionResponse.ok) {
       // 更新対象となるOpenAPI仕様のIDを取得する
       /** @type {string} */
-      const openapiSpecId = await fetchReadMe('GET', '/api-specification', { 'x-readme-version': versionId })
+      const openapiSpecId = await fetchReadMe('GET', '/api-specification', {
+        headers: { 'x-readme-version': versionId }
+      })
         .then(response => Promise.all([response.ok, response.json()]))
         .then(([ok, json]) => ok ? json : Promise.reject(createErrorMessage('Failed to fetch OpenAPI Specs.', json)))
         .then(json => {
@@ -62,7 +67,9 @@ export async function generatePreview({ github, context, core }) {
       const updateOpenAPISpecFormData = new FormData();
       updateOpenAPISpecFormData.append('spec', fileFromSync('./openapi/openapi.yaml'));
 
-      await fetchReadMe('PUT', `/api-specification/${openapiSpecId}`, updateOpenAPISpecFormData)
+      await fetchReadMe('PUT', `/api-specification/${openapiSpecId}`, {
+        body: updateOpenAPISpecFormData
+      })
         .then(response => Promise.all([response.ok, response.json()]))
         .then(([ok, json]) => {
           if (!ok) {
@@ -100,32 +107,30 @@ function convertToSemVer(str) {
  * fetch with ReadMe.com
  * @param {string} method
  * @param {string} path
- * @param {string|stream.Readable} body
+ * @param {Object} params
+ * @param {string|Blob} body
  * @param {Object} headers
  * @returns {Promise<fetch.Response>} response
  */
-function fetchReadMe(method, path, body, headers) {
+function fetchReadMe(method, path, params = {}) {
   const { README_API_KEY } = process.env;
 
   if (typeof README_API_KEY === 'undefined') {
     return Promise.reject('`README_API_KEY` environment variable is not set.');
   }
 
-  const params = {
-    method: method,
-    headers: {
-      'accept': 'application/json',
-      'authorization': `Basic ${Buffer.from(README_API_KEY).toString('base64')}`
-    }
+  params.method = method;
+
+  const headers = {
+    accept: 'application/json',
+    authorization: `Basic ${Buffer.from(README_API_KEY).toString('base64')}`
   };
 
-  if (typeof body !== 'undefined') {
-    params.body = body;
+  if (typeof params.headers !== 'undefined') {
+    Object.assign(headers, params.headers);
   }
 
-  if (typeof headers !== 'undefined') {
-    Object.assign(params.headers, headers);
-  }
+  params.headers = headers;
 
   return fetch('https://dash.readme.com/api/v1' + path, params);
 }
